c9df19c2e7d7c069aeff6cbf3090448825040c44
Tedwards <108353463+Tedwards22890@users.noreply.github.com>
2022-10-12 18:02:55 -0400

    Added modal with proper pop-up

--- src/Components/DisplaySongs/DisplaySongs.jsx
+++ src/Components/DisplaySongs/DisplaySongs.jsx
@@ -1,12 +1,11 @@
 import React, {useState} from 'react';
 import "./DisplaySongs.css";
 import Modal from '../Modal/Modal';
+import axios from 'axios';
 
 const DisplaySongs = (props) => {
     const handleDelete=(i)=>{
-        const deleteValue=[...props]
-        deleteValue.splice(i)
-        setInterval(deleteValue)
+        axios.delete(`http://127.0.0.1:8000/music/${i}/`)
     }
     const [openModal, setOpenModel] = useState(false)
     return ( 
@@ -25,7 +24,10 @@ const DisplaySongs = (props) => {
                             <div className='grid-item'>{songs.album}</div>
                             <div className='grid-item'>{songs.release_date}</div>
                             <div className='grid-item'>{songs.genre}</div>
-                            <div className='grid-item'><button>Edit</button><button onClick={()=>handleDelete(i)}>X</button><Modal open={openModal} /></div>
+                            <div className='grid-item'><button className='modalBtn' onClick={() => setOpenModel(true)}>Edit</button>
+                            <Modal open={openModal} parentSongs={songs} index={i}/>
+                            <button onClick={()=>handleDelete(songs.id)}>X</button>
+                            <Modal open={openModal} onClose={()=>setOpenModel(false)}/></div>
                         </div>
                     </center>
                 </div>
--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,13 +1,83 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './Modal.css'
+import axios from 'axios';
 
-const Modal= ({open}) => {
+const Modal= ({open, onClose}) => {
+
+    const [title, setTitle] = useState('');
+    const [artist, setArtist] = useState('');
+    const [album, setAlbum] = useState('');
+    const [release_date, setRelease_date] = useState('');
+    const [genre, setGenre] = useState('');
+
     if (!open) return null;
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const newSong = {
+            title: title,
+            artist: artist,
+            album: album,
+            release_date: release_date,
+            genre: genre,
+
+        };
+        if (title===''|| artist==='' || album==='' ||release_date===''||genre==='')
+        {
+            alert('Please be sure all fields are filled out!')
+        }
+        else 
+        {
+            console.log({newSong});
+            axios.put('http://127.0.0.1:8000/music/', newSong)
+            setTitle("")
+            setArtist("")
+            setAlbum("")
+            setRelease_date("")
+            setGenre('')
+
+        }
+    }
+    
+
     return (
+        <form onSubmit={handleSubmit} className="form-grid">
         <div className='overlay'>
             <div className='modalContainer'>
-                text
+                <div className="modalRight">
+                    <button className='closeBtn' onClick={onClose}>x</button>
+                    <div className="grid-container">
+                    <div classname="grid-item">
+                    <input type="text" placeholder="Title" value={title} onChange={(event) => setTitle(event.target.value)}/>
+                    </div>
+
+                    <div classname="grid-item">
+
+                    <input type="text" placeholder="Artist" value={artist} onChange={(event) => setArtist(event.target.value)}/>
+                    </div>
+
+                    <div classname="grid-item">
+
+                    <input type="text" placeholder="Album" value={album} onChange={(event) => setAlbum(event.target.value)}/>
+                    </div>
+
+                    <div classname="grid-item">
+
+                    <input type="date" placeholder="Release Date" value={release_date} onChange={(event) => setRelease_date(event.target.value)}/>
+                    </div>
+
+                    <div classname="grid-item">
+
+                    <input type="text" placeholder="Genre" value={genre} onChange={(event) => setGenre(event.target.value)}/>
+                    </div>
+                <br/>
+                </div>
+                    <button type="submit" className="btn btn-primary" style={{"margin-right": "1em"}}>Update</button>
+
+                </div>
             </div>
         </div>
+        </form>
     )
 }
 
